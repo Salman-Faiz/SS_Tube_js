ffc58eaf7cc31acd53b3c8101777356208028da5
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-04-26 20:39:05 +0600

    blogs ques1 added

---
+++ js/utilities.js
@@ -0,0 +1,7 @@
+const btn_blogs = document.getElementById('btn_blogs')
+btn_blogs.addEventListener('click',function btn_blogs(){
+
+    // console.log('hellloo')
+    window.location.href = "http://127.0.0.1:5500/blogs.html"
+    
+})
