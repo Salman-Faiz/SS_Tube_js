9d6f484771d124ebee98792571ef2a2a951f200b
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-04-27 07:44:59 +0600

    fetch card data and show in grid

--- js/utilities.js
+++ js/utilities.js
@@ -13,14 +13,50 @@ const loadData = async () => {
 
     const dynamicBtn = document.getElementById('dynamicBtn');
     dynamicBtn.innerHTML = `
-        <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
-        <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
+        <button onclick="showAll(${data?.data[0]?.category_id})" class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
+        <button  class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
         <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[2]?.category}</button>
         <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[3]?.category}</button>`;
+
 
 }
 
 loadData();
+
+// all btn show data
+
+const showAll = async(category_id) =>{
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
+    const data = await response.json();
+    // console.log(data.data);
+    const cardData =data.data;
+    const cardDiv =document.getElementById('cardContainer')
+    
+    cardData.forEach(card => {
+        console.log(card);
+        const singleDiv = document.createElement('div')
+        singleDiv.innerHTML=` <div>
+               
+        <img class="rounded-md" src="${card.thumbnail}" alt="">
+    
+         <div class="space-y-2 flex gap-3 pt-4">
+        <img src="${card?.profile_picture}" alt="">
+        <div class="flex-1">
+          <h1 class="text-2xl font-semibold">${card.title}</h1>
+         <div class="flex gap-4">
+        <p>${card.authors[0].profile_name}</p>
+        <p><i class="fa-solid fa-certificate"></i></p>
+       </div>  
+       <p>${card.others?.views}</p>
+        </div>
+        </div>
+        </div>`
+cardDiv.appendChild(singleDiv);
+        
+    });
+
+}
 
 
 
