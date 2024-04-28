291a8648d164e99d6c2f80e4c612745cf12328e6
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-04-28 22:22:58 +0600

    applied ternary operator for blue batch & falsy data access show not found alert $ correction codes

--- js/utilities.js
+++ js/utilities.js
@@ -13,10 +13,10 @@ const loadData = async () => {
 
     const dynamicBtn = document.getElementById('dynamicBtn');
     dynamicBtn.innerHTML = `
-        <button onclick="showAll(${data?.data[0]?.category_id})" class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
-        <button  class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
-        <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[2]?.category}</button>
-        <button class="px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[3]?.category}</button>`;
+        <button  onclick="showAll(${data?.data[0]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
+        <button onclick="showAll(${data?.data[1]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
+        <button onclick="showAll(${data?.data[2]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[2]?.category}</button>
+        <button onclick="showAll(${data?.data[3]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[3]?.category}</button>`;
 
 
 }
@@ -25,38 +25,67 @@ loadData();
 
 // all btn show data
 
-const showAll = async(category_id) =>{
-
+const showAll = async (category_id = '1000') => {
+    let cardData = [];
+    let isEmpty = false;
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
     const data = await response.json();
-    // console.log(data.data);
-    const cardData =data.data;
-    const cardDiv =document.getElementById('cardContainer')
+    console.log(data.data);
     
-    cardData.forEach(card => {
-        console.log(card);
-        const singleDiv = document.createElement('div')
-        singleDiv.innerHTML=` <div>
-               
-        <img class="rounded-md h-60 w-96" src="${card.thumbnail}" alt="">
-    
-         <div class="space-y-2 flex gap-3 pt-4">
-         <img class="rounded-full w-14 h-14 mt-5 " src="${card.authors[0].profile_picture}" alt="">
-        <div class="flex-1">
-          <h1 class="text-2xl font-semibold">${card.title}</h1>
-         <div class="flex gap-4">
-        <p>${card.authors[0].profile_name}</p>
-        <p><i class="fa-solid fa-certificate"></i></p>
-       </div>  
-       <p>${card.others?.views}</p>
-        </div>
-        </div>
-        </div>`
-cardDiv.appendChild(singleDiv);
-        
-    });
+   
+    const cardDiv = document.getElementById('cardContainer')
+    // to prevent multiple clicks
+    cardDiv.textContent = '';
 
+    if(data.status){
+        cardData = data.data; 
+
+        cardData?.forEach(card => {
+            console.log(card);
+            const singleDiv = document.createElement('div')
+    
+    
+            singleDiv.innerHTML = ` <div>
+                   
+            <img class="rounded-md h-60 w-96" src="${card.thumbnail}" alt="">
+        
+             <div class="space-y-2 flex gap-3 pt-4">
+             <img class="rounded-full w-14 h-14 mt-5 " src="${card.authors[0].profile_picture}" alt="">
+            <div class="flex-1">
+              <h1 class="text-2xl font-semibold">${card.title}</h1>
+             <div id="blueVerified" class="flex gap-4">
+            <p>${card.authors[0].profile_name}</p>
+            <div> ${card.authors[0]?.verified ? "<i class='fa-solid fa-certificate'></i>":""}</div>
+           </div>  
+           <p>${card.others?.views}</p>
+            </div>
+            </div>
+            </div>`
+            // <i class="fa-solid fa-certificate"></i>
+            cardDiv.appendChild(singleDiv);
+    
+        });
+       
+    }
+    else{
+        const errorDiv = document.getElementById('errorDiv')
+        
+       
+        errorDiv.innerHTML =`  <div  class="flex justify-center align-center">
+        <img class="" src="resources/Icon.png" alt="">
+        
+      </div>
+      <h3 class="text-center">Opps ,There is no data available</h3>  `
+        
+
+        
+    }
+
+   
+   
+   
 }
+showAll();
 
 
 
