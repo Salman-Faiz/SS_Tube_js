f85db61381e22bedfccbf97c5bdbd3f712502014
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-05-01 15:08:10 +0600

    code correction

--- js/utilities.js
+++ js/utilities.js
@@ -1,23 +1,21 @@
-const btn_blogs = document.getElementById('btn_blogs')
-btn_blogs.addEventListener('click', function btn_blogs() {
+const readBlogs = () => {
+  window.location.href = "http://127.0.0.1:5500/blogs.html"
 
-    // console.log('hellloo')
-    window.location.href = "http://127.0.0.1:5500/blogs.html"
-
-})
+}
 
 const loadData = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const data = await response.json();
-    // console.log(data.data[0].category);
-
-    const dynamicBtn = document.getElementById('dynamicBtn');
-    dynamicBtn.innerHTML = `
-        <button  onclick="showAll(${data?.data[0]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
-        <button onclick="showAll(${data?.data[1]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
-        <button onclick="showAll(${data?.data[2]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[2]?.category}</button>
-        <button onclick="showAll(${data?.data[3]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[3]?.category}</button>`;
-
+  const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+  const data = await response.json();
+  // console.log(data.data[0].category);
+  showButtons(data);
+}
+const showButtons = (data) => {
+  const dynamicBtn = document.getElementById('dynamicBtn');
+  dynamicBtn.innerHTML = `
+      <button  onclick="showAll(${data?.data[0]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold                    text-white">${data?.data[0]?.category}</button>
+      <button onclick="showAll(${data?.data[1]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[1]?.category}</button>
+      <button onclick="showAll(${data?.data[2]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[2]?.category}</button>
+      <button onclick="showAll(${data?.data[3]?.category_id})" class="hover:bg-red-600 px-4 py-3 bg-slate-400 rounded-md mt-4 text-center text-2xl font-semibold text-white">${data?.data[3]?.category}</button>`;
 
 }
 
@@ -26,32 +24,32 @@ loadData();
 // all btn show data
 
 const showAll = async (category_id = '1000') => {
-    let cardData = [];
-    let sortDiv =[];
-    let views =[];
-    let isEmpty = false;
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
-    const data = await response.json();
-    // console.log(data.data);
-    
-   
-    const cardDiv = document.getElementById('cardContainer')
-    // to prevent multiple clicks
-    cardDiv.textContent = '';
+  let cardData = [];
+  let sortDiv = [];
+  let views = [];
+  let isEmpty = false;
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
+  const data = await response.json();
+  // console.log(data.data);
 
-    if(data.status){
-        cardData = data.data; 
 
-        cardData?.forEach(card => {
-            // console.log(card);
-            const singleDiv = document.createElement('div')
-    
-    
-            singleDiv.innerHTML = ` <div>
+  const cardDiv = document.getElementById('cardContainer')
+  // to prevent multiple clicks
+  cardDiv.textContent = '';
+
+  if (data.status) {
+    cardData = data.data;
+
+    cardData?.forEach(card => {
+      // console.log(card);
+      const singleDiv = document.createElement('div')
+
+
+      singleDiv.innerHTML = ` <div>
 
             <div class="">
                     <img class="rounded-md h-60 w-96" src="${card.thumbnail}" alt="">
-                        <h3 class="text-white font-bold text-end relative bottom-10 pe-10 rounded-sm">${card.others?.posted_date ? (card.others?.posted_date /60 )/60 :""}</h3>
+                        <div class="text-white font-bold text-end relative bottom-10 pe-10 rounded-sm">${card.others?.posted_date ? (card.others?.posted_date / 60) / 60 : ""}</div>
                 </div>
              <div class="space-y-2 flex gap-3 pt-4">
              <img class="rounded-full w-14 h-14 mt-5 " src="${card.authors[0].profile_picture}" alt="">
@@ -59,45 +57,33 @@ const showAll = async (category_id = '1000') => {
               <h1 class="text-2xl font-semibold">${card.title}</h1>
              <div id="blueVerified" class="flex gap-4">
             <p>${card.authors[0].profile_name}</p>
-            <div> ${card.authors[0]?.verified ? "<i class='fa-solid fa-certificate'></i>":""}</div>
+            <div> ${card.authors[0]?.verified ? "<i class='fa-solid fa-certificate'></i>" : ""}</div>
            </div>  
            <p>${card.others?.views}</p>
            
             </div>
             </div>
             </div>`
-            // <i class="fa-solid fa-certificate"></i>
-            cardDiv.appendChild(singleDiv);
-    
-        });
-       
-    }
-    else{
-        const errorDiv = document.getElementById('errorDiv')
-        
-       
-        errorDiv.innerHTML =`  <div  class="flex justify-center align-center">
+      // <i class="fa-solid fa-certificate"></i>
+      cardDiv.appendChild(singleDiv);
+
+    });
+
+  }
+  else {
+    const errorDiv = document.getElementById('errorDiv')
+
+
+    errorDiv.innerHTML = `  <div  class="flex justify-center align-center">
         <img class="" src="resources/Icon.png" alt="">
         
       </div>
       <h3 class="text-center text-4xl font-bold">Opps ,There is no data available</h3>  `
-        
 
-        
-    }
 
-  sortDiv = data.data;
-//   console.log('hello',sortDiv); 
-//   console.log('ffff',sortDiv[1].others?.views); 
-  sortDiv?.map(element => {
-  
-//    console.log(element.others.views);
-   
-   views=element.others.views;
-  //  console.log(views);
-  views = sortDiv.toSorted();
-  console.log(views);
-  });
+
+  }
+
 }
 showAll();
 
